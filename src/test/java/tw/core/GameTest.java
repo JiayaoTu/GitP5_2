b92a3aacef0de078154858274888aaa8447e7c97
Kingtanna <15109256726@163.com>
2018-04-21 17:41:00 +0800

    GameTest

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,5 +1,13 @@
 package tw.core;
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.AnswerGenerator;
+import tw.validator.InputValidator;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import org.junit.Test;
 
 /**
@@ -8,7 +16,37 @@ import org.junit.Test;
 
 
 public class GameTest {
-    @Test
-    public void name() {
+
+    private Answer answer = Answer.createAnswer("1 2 3 4");
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
+        game = new Game(answerGenerator);
     }
+
+    @Test
+    public void should_get_success_status_when_input_str_correct() throws Exception {
+
+
+        game.guess(Answer.createAnswer("1 2 3 4"));
+
+        String statusOfGame = game.checkStatus();
+        assertThat(statusOfGame, is("success"));
+
+    }
+
+    @Test
+    public void should_get_continue_status_when_input_str_incorrect() throws Exception {
+
+        game.guess(Answer.createAnswer("1 2 4 3"));
+
+
+        String statusOfGame = game.checkStatus();
+        assertThat(statusOfGame, is("continue"));
+
+    }
+
 }
